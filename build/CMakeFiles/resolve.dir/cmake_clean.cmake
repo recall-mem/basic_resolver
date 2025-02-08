e28ead6a14b94f029b3ad0285badb1f05eb592f2
recall-mem <abbas_3211@mail.ru>
2025-02-09 02:27:59 +0400

    Add files via upload

---
+++ build/CMakeFiles/resolve.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resolve.dir/resolver.cpp.o"
+  "CMakeFiles/resolve.dir/resolver.cpp.o.d"
+  "resolve"
+  "resolve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resolve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
