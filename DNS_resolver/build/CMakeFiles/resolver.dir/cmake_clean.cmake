4d56f10c1be921188085649e6250c8042ccba649
recall-mem <abbas_3211@mail.ru>
2025-02-09 02:25:24 +0400

    Add files via upload

---
+++ DNS_resolver/build/CMakeFiles/resolver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resolver.dir/resolver.cpp.o"
+  "CMakeFiles/resolver.dir/resolver.cpp.o.d"
+  "resolver"
+  "resolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
